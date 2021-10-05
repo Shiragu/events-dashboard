6e79cb3842b5029c9ab04ea8dcc37c3483992493
Shiragu <pipboy2009@yandex.ru>
2021-10-05 20:21:18 +0300

    feat: added all events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,16 @@
+import EventList from '../../components/events/eventList';
+import { getAllEvents } from '../../data/test_data';
+import { css } from "@emotion/css";
+
 function EventsPage() {
+  const allEvents = getAllEvents();
+
   return (
     <div>
-      <h1>Список событий</h1>
+      <h1 className={css`
+          text-align: center;
+        `}>Все события</h1>
+      <EventList events={allEvents}  />
     </div>
   );
 }
