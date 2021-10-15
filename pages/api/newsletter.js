a2b654039e3dbaf79bd083dd30502ce730ece3b3
Shiragu <pipboy2009@yandex.ru>
2021-10-15 22:38:52 +0300

    feat: established mongodb connection

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
 
@@ -6,6 +8,13 @@ function handler(req, res) {
       res.status(422).json({ message: "Неверный адрес email!" });
       return;
     }
+
+    const client = await MongoClient.connect("url");
+    const db = client.db();
+
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    client.close();
 
     console.log(userEmail);
     res.status(201).json({ message: "Вы подписались!" });
